86e01074ff896f83ac2fc9dcbff77ebf4d99c33d
jayanth-gh <sivasaijayanth_illuru@srmap.edu.in>
2026-06-14 16:28:26 +0530

    mod url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://true-sight-ai.onrender.com/api',
 });
 
 api.interceptors.request.use(
